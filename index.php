3eddc5c06b99f129cbf3dfae657543e027c96ca6
Corfaya <lauracorrado0696@gmail.com>
2024-09-17 16:48:52 +0200

    [UPD] Bonus 2 DONE

--- index.php
+++ index.php
@@ -9,21 +9,42 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css/style.css">
     <title>PHP - OOP Class exercise</title>
 </head>
 <body>
     <div class="container">
-        <div class="row">
+        <div class="row gy-3">
             <div class="col-12">
-                <h1>PHP Movie Class</h1>
+                <h1 class="my-3 text-center fw-bolder">PHP Movie List</h1>
             </div>
+            <?php foreach($moviesArray as $movie) { ?>
             <div class="col-12 col-md-6 col-4">
-                <div class="card">
-                    <div class="card-header">
-                        <h2> </h2>
+                <div class="card h-100 p-3">
+                    <div class="card-title">
+                        <h2> <?php echo $movie->title ?> </h2>
+                    </div>
+                    <div class="card-body">
+                        <h4>Genere:</h4>
+                        <?php foreach($movie->genres as $genre) { ?>
+                            <p> <?php echo $genre ?> </p>
+                        <?php } ?>
+                        <h4>Durata:</h4>
+                        <p> <?php echo $movie->duration ?> minuti</p>
+                        <h4>Regista:</h4>
+                        <p> <?php echo $movie->director ?> </p>
+                        <h4>Anno:</h4>
+                        <p> <?php echo $movie->year ?> </p>
+                        <h4>Cast principale:</h4>
+                        <ul class="list-unstyled">
+                            <?php foreach($movie->cast as $actor) { ?>
+                                <li><?php echo $actor ?></li>
+                            <?php } ?>
+                        </ul>
                     </div>
                 </div>
             </div>
+            <?php } ?>
         </div>
     </div>
 </body>
--- partials/db.php
+++ partials/db.php
@@ -1,10 +1,11 @@
 <?php
     include "./Models/Movie.php";
     // objects
-    $movie1 = new Movie("...altrimenti ci arrabbiamo!", ["Commedia", "Azione"], 98, "Marcello Fondato", 1974, ["Terence Hill", "Bud Spencer", "John Sharp", "Patty Shepard", "Donald Pleasence"]);
-    $movie2 = new Movie("Paura e delirio a Las Vegas", ["Commedia", "Grottesco", "Drammatico"], 118, "Terry Gilliam", 1998, ["Johnny Depp", "Benicio del Toro", "Tobey Maguire", "Ellen Barkin", "Christina Ricci"]);
+    $movie1 = new Movie("Paura e delirio a Las Vegas", ["Commedia", "Grottesco", "Drammatico"], 118, "Terry Gilliam", 1998, ["Johnny Depp", "Benicio del Toro", "Tobey Maguire", "Ellen Barkin", "Christina Ricci"]);
+    $movie2 = new Movie("...altrimenti ci arrabbiamo!", ["Commedia", "Azione"], 98, "Marcello Fondato", 1974, ["Terence Hill", "Bud Spencer", "John Sharp", "Patty Shepard", "Donald Pleasence"]);
     $movie3 = new Movie("The Northman", ["Avventura", "Azione", "Drammatico", "Storico", "Fantastico"], 137, "Robert Eggers", 2022, ["Alexander Skarsgård", "Claes Bang", "Nicole Kidman", "Willem Defoe", "Ethan Hawke", "Anya Taylot-Joy"]);
+    $movie4 = new Movie("Beetlejuice", ["commedia", "fantastico", "grottesco"], 92, "Tim Burton", 1988, ["Michael Keaton", "Alec Baldwin", "Winona Ryder", "Catherine O'Hara"]) ;
 
-    $moviesArray = [$movie1, $movie2, $movie3];
-    var_dump($moviesArray)
+    $moviesArray = [$movie1, $movie2, $movie3, $movie4];
+    // var_dump($moviesArray)
 ?>
