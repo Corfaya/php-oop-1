978a4e5b2302375ad26aee0928e31c8a5e608d24
Corfaya <lauracorrado0696@gmail.com>
2024-09-17 15:12:04 +0200

    [UPD] constructor function assignments

--- index.php
+++ index.php
@@ -8,7 +8,12 @@
         public $cast; // array
 
         function __construct ($_title, $_genre, $_duration, $_director, $_year, Array $_cast) {
-            
+            $this->title = $_title;
+            $this->genre = $_genre;
+            $this->duration = $_duration;
+            $this->director = $_director;
+            $this->year = $_year;
+            $this->cast = $_cast;
         }
     }
 ?>
