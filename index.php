5c8b942bd14a2c6faabd5fafa6403ac1572b1557
Corfaya <lauracorrado0696@gmail.com>
2024-09-17 15:38:58 +0200

    [UPD] objects created from Movie class

--- index.php
+++ index.php
@@ -21,4 +21,9 @@
             return $this->title." ".$this->genre." ".$this->duration." ".$this->director." ".$this->year." ".$this->cast;
         }
     }
+
+    // objects
+    $altrimenti_ci_arrabbiamo = new Movie("...altrimenti ci arrabbiamo!", "Commedia", 98, "Marcello Fondato", 1974, ["Terence Hill", "Bud Spencer", "John Sharp", "Patty Shepard", "Donald Pleasence"]);
+    $paura_e_delirio = new Movie("Paura e delirio a Las Vegas", "commedia", 118, "Terry Gilliam", 1998, ["Johnny Depp", "Benicio del Toro", "Tobey Maguire", "Ellen Barkin", "Christina Ricci"]);
+    $northman = new Movie("The Northman", "avventura", 137, "Robert Eggers", 2022, ["Alexander Skarsgård", "Claes Bang", "Nicole Kidman", "Willem Defoe", "Ethan Hawke", "Anya Taylot-Joy"]);
 ?>
